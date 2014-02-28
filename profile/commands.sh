543031f3c389b7d3adb6e5bc0d5740daaf558bfa
Marcos Tapajós <tapajos@gmail.com>
2014-02-28 13:43:08 -0300

    create kill all ruby function

--- profile/commands.sh
+++ profile/commands.sh
@@ -2,5 +2,9 @@ alias dns="sudo killall -HUP mDNSResponder"
 alias ls="ls -h"
 
 function remove_whitespaces {
-  find * -type f \( -name '*.rb' -o -name '*.js' -o -name "*.erb" -o -name "*.haml" \) -print -exec perl -pi -e 's/[\t ]+$//g' {} \;
+	find * -type f \( -name '*.rb' -o -name '*.js' -o -name "*.erb" -o -name "*.haml" \) -print -exec perl -pi -e 's/[\t ]+$//g' {} \;
+}
+
+function killruby {
+	pgrep '^ruby*' | xargs kill -9
 }

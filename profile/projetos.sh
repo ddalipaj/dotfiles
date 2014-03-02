ee498896d7f3ea20a7eddcf84625e383d491dda4
Marcos Tapajós <tapajos@gmail.com>
2014-03-02 12:45:18 -0300

    updating

--- profile/projetos.sh
+++ profile/projetos.sh
@@ -1,1 +1,28 @@
-alias p="cd $HOME/workspace"
+alias p="cd $PROJECTS_PATH"
+
+function update_all {
+  source $HOME/dotfiles/profile/chruby.sh
+  cd $PROJECTS_PATH
+  for i in $(find . -type d); do
+    if [ -d "$i/.git" ]; then
+      echo -e "\033[1mPulling "$i"\033[0m"
+      cd "$i"
+      # pull repos without local changes
+      if git diff-index --quiet HEAD --; then
+        git pull --rebase --quiet > /dev/null
+        if [ -e Gemfile ]; then
+          echo -e "\033[1m'Bundling' "$i"\033[0m"
+          bundle --local --quiet > /dev/null
+        fi
+        git remote prune origin > /dev/null
+        git gc --quiet > /dev/null
+      fi
+      cd - > /dev/null
+    fi
+  done
+}
+
+function truncate_logs {
+  for item in $(find $PROJECTS_PATH -name '*.log' -type f) ;do cat /dev/null > $item ;done
+  for item in $(find `brew --prefix`/var/log -name '*.log' -type f) ;do cat /dev/null > $item ;done
+}

0871a2aa66691309529ec2d002799b139b442e5b
Marcos <tapajos@gmail.com>
2014-07-31 00:43:44 -0300

    update

--- profile/12_scm.sh
+++ profile/12_scm.sh
@@ -13,9 +13,9 @@ alias gm="git checkout master"
 
 
 if [[ -n "$GIT_USER_NAME" ]]; then
-  git config --global user.name $GIT_USER_NAME
+  git config --global user.name "$GIT_USER_NAME"
 fi
 
 if [[ -n "$GIT_USER_EMAIL" ]]; then
-  git config --global user.email $GIT_USER_EMAIL
+  git config --global user.email "$GIT_USER_EMAIL"
 fi

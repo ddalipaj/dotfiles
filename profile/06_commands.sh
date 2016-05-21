8f0c09d05c256d1a895a075f8c029b7fd0297039
Marcos Tapajós <tapajos@gmail.com>
2016-05-21 16:43:21 -0300

    Atom

--- profile/06_commands.sh
+++ profile/06_commands.sh
@@ -1,5 +1,6 @@
 alias e="$TEXT_EDITOR"
 alias ls="ls -h"
+alias myip="curl whatismyip.akamai.com"
 
 function remove_whitespaces {
   find * -type f \( -name '*.rb' -o -name '*.js' -o -name "*.erb" -o -name "*.haml" \) -print -exec perl -pi -e 's/[\t ]+$//g' {} \;

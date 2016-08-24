d424e52f6859fa7baf4df90f7fe09f5a3a21ac7d
Marcos Tapajós <tapajos@gmail.com>
2016-08-24 13:29:04 -0300

    update

--- profile/04_chruby.sh
+++ profile/04_chruby.sh
@@ -2,7 +2,7 @@ if [ -f /usr/local/bin/brew ]; then
   if [[ -e `brew --prefix`/share/chruby ]]; then
     # Load chruby
     source `brew --prefix`/share/chruby/chruby.sh
-    chruby ruby-2.2.3
+    chruby ruby-2.3.1
     # Automatically switch rubies
     source `brew --prefix`/share/chruby/auto.sh
   fi
